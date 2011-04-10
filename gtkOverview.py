f5984b4f95090aef0353b5a979eebcb498f9d789
Vincent Berthoux <twinside@gmail.com>
2011-04-10 22:14:03 +0200

    Vain atempt to send keys back to vim

--- gtkOverview.py
+++ gtkOverview.py
@@ -6,6 +6,170 @@ import gtk
 import gio
 import sys
 import cairo
+from ctypes import *
+
+class XScreen(Structure):
+    _fields_ = [
+        ("ext_data", c_void_p),
+        ("display", c_void_p),
+        ("root", c_int),
+        ("width", c_int),
+        ("height", c_int),
+        ("mwidth", c_int),
+        ("mheight", c_int),
+        ("ndepths", c_int),
+        ("depths", c_void_p),
+        ("root_depth", c_int),
+        ("root_visual", c_void_p),
+        ("default_gc", c_void_p),
+        ("cmap", c_int),
+        ("white_pixel", c_long),
+        ("black_pixel", c_long),
+        ("max_maps", c_int),
+        ("min_maps", c_int),
+        ("backing_store", c_int),
+        ("save_unders", c_bool),
+        ("root_input_mask", c_long)]
+
+class XDisplay(Structure):
+    _fields_ = [
+        ("ext_data", c_void_p),
+        ("private1", c_void_p),
+        ("fd", c_int),
+        ("private2", c_int),
+        ("proto_major_version", c_int),
+        ("proto_minor_version", c_int),
+        ("vendor", c_void_p),
+        ("private3", c_int),
+        ("private4", c_int),
+        ("private5", c_int),
+        ("private6", c_int),
+        ("resource_alloc", c_void_p),
+        ("byte_order", c_int),
+        ("bitmap_unit", c_int),
+        ("bitmap_pad", c_int),
+        ("bitmap_bit_order", c_int),
+        ("nformats", c_int),
+        ("pixmap_format", c_void_p),
+        ("private8", c_int),
+        ("release", c_int),
+        ("private9", c_void_p),
+        ("private10", c_void_p),
+        ("qlen", c_int),
+        ("last_request_read", c_long),
+        ("request", c_long),
+        ("private11", c_void_p),
+        ("private12", c_void_p),
+        ("private13", c_void_p),
+        ("private14", c_void_p),
+        ("max_request_size", c_int),
+        ("db", c_void_p),
+        ("private15", c_void_p),
+        ("display_name", c_void_p),
+        ("default_screen", c_int),
+        ("nscreens", c_int),
+        ("screens", POINTER(XScreen)),
+        ("motion_buffer", c_long),
+        ("private16", c_long),
+        ("min_keycode", c_int),
+        ("max_keycode", c_int),
+        ("private17", c_void_p),
+        ("private18", c_void_p),
+        ("private19", c_int),
+        ("xdefaults", c_void_p)]
+
+class XEvent(Structure):
+    _fields_ = [("keytype", c_int),
+                ("serial", c_ulong),
+                ("send_event", c_bool),
+                ("display", POINTER(XDisplay)),
+                ("window", c_int),
+                ("root", c_int),
+                ("subwindow", c_int),
+                ("time", c_int),
+                ("x", c_int),
+                ("y", c_int),
+                ("x_root", c_int),
+                ("y_root", c_int),
+                ("state", c_int),
+                ("keycode", c_int),
+                ("same_screen", c_bool)]
+
+class XKeySender:
+    def __init__(self, windowId):
+        self.windowId = windowId
+        self.lib = CDLL("libX11.so.6")
+
+        self.key_g = 0x67
+        self.key_z = 0x7A
+
+        # Keys for all the numbers
+        self.key_num = range(0x30, 0x40)
+
+    def openDisplay(self):
+        xOpenDisplay = self.lib.XOpenDisplay
+        xOpenDisplay.restype = POINTER(XDisplay)
+        return xOpenDisplay(c_char_p(0))
+
+    def rootWindow(self, display):
+        defaultScreen = display.contents.default_screen
+        screen = display.contents.screens[defaultScreen]
+        return screen.root
+        
+    def sendChar(self, display, keySim):
+        keyPressMask = 1
+        keyPress = 2
+        keyRelease = 3
+        none = 0
+        currentTime = 0
+        keyCode = self.lib.XKeysymToKeycode(display, c_int(keySim))
+        event = XEvent( keyPress # type
+                      , 0 # serial
+                      , 0 # send_event
+                      , display # display
+                      , self.windowId # window
+                      , self.rootWindow(display) # rootwindow
+                      , none #subwindow
+                      , currentTime # time
+                      , 1 # x
+                      , 1 # y
+                      , 1 #xroot
+                      , 1 #yroot
+                      , 0 #state
+                      , keyCode #keycode
+                      , 1 #same_screen )
+                      )
+
+        ret = self.lib.XSendEvent(display, 
+                            c_int(self.windowId),
+                            c_bool(True),
+                            c_int(keyPressMask),
+                            byref(event))
+
+        print(ret)
+        event.eventType = keyRelease
+
+        ret = self.lib.XSendEvent(display, 
+                            c_int(self.windowId),
+                            c_bool(True),
+                            c_int(keyPressMask),
+                            byref(event))
+        print(ret)
+        self.lib.XSync(display, 1)
+
+    def sendLineChange(self, line):
+        display = self.openDisplay()
+
+        for c in str(line):
+            self.sendChar(display, self.key_num[int(c)])
+
+        self.sendChar(display, self.key_g)
+        self.sendChar(display, self.key_g)
+
+        self.sendChar(display, self.key_z)
+        self.sendChar(display, self.key_z)
+
+        self.lib.XCloseDisplay(display)
 
 class OverViewImage:
     # when invoked (via signal delete_event), terminates the application.
@@ -14,8 +178,11 @@ class OverViewImage:
         return False
 
     # is invoked when the button is clicked.  It just prints a message.
-    def button_clicked(self, widget, data=None):
-        print "button %s clicked" % data
+    def image_clicked(self, widget, data=None):
+        if not self.initiated:
+            return
+        yPos = int(data.y)
+        #self.keySender.sendLineChange(yPos)
 
     def updateViewSizeInformation(self, picture):
         (width, height) = (picture.get_width(), picture.get_height())
@@ -56,11 +223,12 @@ class OverViewImage:
         self.beginning = int(begin)
         self.ending = int(end)
         self.updateImage(imageFile)
-        self.vimWinId = int(winId)
+        self.keySender.windowId = int(winId)
 
         self.backColor = gtk.gdk.color_parse(backColor)
         self.window.modify_bg(gtk.STATE_NORMAL, self.backColor)
 
+        self.rectColor = gtk.gdk.color_parse(viewRectColor)
         self.initiated = True
 
     def area_draw(self, area, event):
@@ -82,7 +250,9 @@ class OverViewImage:
         cr.set_source_pixbuf(self.scaledPixbuf, 0, 0)
         cr.paint()
 
-        cr.set_source_rgba(0.7, 0.7, 1.0, 0.6)
+        cr.set_source_rgba(self.rectColor.red_float, 
+                           self.rectColor.green_float, 
+                           self.rectColor.blue_float, 0.6)
         cr.rectangle(0, int(self.realTop), width, int(height))
         cr.fill()
         
@@ -109,10 +279,15 @@ class OverViewImage:
         self.backColor = gtk.gdk.color_parse('#FFFFFF')
 
         self.drawArea = gtk.DrawingArea()
+        self.drawArea.set_events(gtk.gdk.BUTTON_PRESS_MASK | gtk.gdk.BUTTON_RELEASE_MASK)
         self.drawArea.connect("expose-event", self.area_draw )
+        self.drawArea.connect("button-release-event", self.image_clicked )
+        #self.drawArea.connect("button-press-event", self.image_clicked )
         self.drawArea.show()
 
         self.window.add(self.drawArea)
+
+        self.keySender = XKeySender(0)
 
 if __name__ == "__main__":
     watchedFilename = "/tmp/overviewFile" + sys.argv[1] + '.txt'
